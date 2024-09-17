d56023388764b6335ec97190abca45c156b1087b
ShadowGhost25 <ilyushavavi@gmail.com>
2024-09-17 15:42:11 +0300

    add MainPage

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Footer />
+    </>
+  )
+}
+
+export default MainPage
